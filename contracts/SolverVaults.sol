04d18ba2b8e61815ab67d787f0ae385570256a7b
Navid <mojtaba.farokhi99@gmail.com>
2023-12-11 18:31:21 +0330

    Replace AccessControlUpgradeable with AccessControlEnumerableUpgradeable

--- contracts/SolverVaults.sol
+++ contracts/SolverVaults.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.20;
 
-import "@openzeppelin/contracts-upgradeable/access/AccessControlUpgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/access/extensions/AccessControlEnumerableUpgradeable.sol";
 import "@openzeppelin/contracts-upgradeable/utils/PausableUpgradeable.sol";
 import "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
 import "@openzeppelin/contracts/token/ERC20/extensions/IERC20Metadata.sol";
@@ -11,7 +11,7 @@ import "hardhat/console.sol";
 
 contract SolverVault is
 Initializable,
-AccessControlUpgradeable,
+AccessControlEnumerableUpgradeable,
 PausableUpgradeable
 {
     bytes32 public constant DEPOSITOR_ROLE = keccak256("DEPOSITOR_ROLE");
